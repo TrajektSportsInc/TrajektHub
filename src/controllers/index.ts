8c9d7f368796c6fd3586e3af49e1f48d237a2ac3
Carl Wong <carl@zbae>
2024-11-26 11:38:43 -0500

    delete sample stuff

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,7 @@
 import node from './node.controller';
-import sample from './sample.controller';
 import tracking from './tracking.controller';
 
 export default {
   Node: node,
-  Sample: sample,
   Tracking: tracking,
 };
