fef33f048ba1894c0994377a83b64df2b6bf6ef9
Carl Wong <carl@zbae>
2025-03-18 11:04:12 -0400

    add trackman interfaces

---
+++ src/interfaces/trackman.ts
@@ -0,0 +1,54 @@
+export interface ISessionData {
+  Version: string;
+  Time: string;
+  SessionId: string;
+  SessionType: string;
+  Location: Location;
+  Batters: Batter[];
+  Pitchers: Pitcher[];
+  SessionState: SessionState;
+}
+
+export interface ISession {
+  id: string;
+  subject: string;
+  data: ISessionData;
+  eventType: string;
+  dataVersion: string;
+  metadataVersion: string;
+  eventTime: string;
+  topic: string;
+}
+
+interface Location {
+  Venue: Venue;
+  Field: Field;
+}
+
+interface Venue {
+  Name: string;
+}
+
+interface Field {
+  Name: string;
+}
+
+interface Batter {
+  ForeignId: string;
+  // format: "[last name], [first name]"
+  NameRef: string;
+}
+
+interface Pitcher {
+  ForeignId: string;
+  // format: "[last name], [first name]"
+  NameRef: string;
+}
+
+interface SessionState {
+  State: string;
+  SessionStartedUtc: string;
+  SessionStartedLocal: string;
+  SessionEndedUtc: string;
+  SessionEndedLocal: string;
+}
